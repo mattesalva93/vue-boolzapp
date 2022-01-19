2264eef7b48593d42fa7f70cbda3b0dce820d3d5
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-01-19 18:26:22 +0100

    milestone 1 done

--- myscript.js
+++ myscript.js
@@ -1,6 +1,109 @@
 let app = new Vue ({
     el: '#miapp',
     data: {
+        contacts: [
+            {
+            name: 'Nonna',
+            avatar: 'img/img_01.jpg',
+            visible: true,
+            messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                text: 'Hai portato a spasso il cane?',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                text: 'Ricordati di dargli da mangiare',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 16:15:22',
+                text: 'Tutto fatto!',
+                status: 'received'
+                }
+                ],
+            },
+            {
+            name: 'Franco',
+            avatar: 'img/img_02.jpg',
+            visible: true,
+            messages: [
+                {
+                date: '20/03/2020 16:30:00',
+                text: 'Ciao come stai?',
+                status: 'sent'
+                },
+                {
+                date: '20/03/2020 16:30:55',
+                text: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+                },
+                {
+                date: '20/03/2020 16:35:00',
+                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'sent'
+                }
+                ],
+            },
+            {
+            name: 'Samuele',
+            avatar: 'img/img_03.jpg',
+            visible: true,
+            messages: [
+                {
+                date: '28/03/2020 10:10:40',
+                text: 'La Marianna va in campagna',
+                status: 'received'
+                },
+                {
+                date: '28/03/2020 10:20:10',
+                text: 'Sicuro di non aver sbagliato chat?',
+                status: 'sent'
+                },
+                {
+                date: '28/03/2020 16:15:22',
+                text: 'Ah scusa!',
+                status: 'received'
+                }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: 'img/img_04.jpg',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                text: 'Lo sai che ha aperto una nuova pizzeria?',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                text: 'Si, ma preferirei andare al cinema',
+                status: 'received'
+                }
+                ],
+            },
+            {
+                name: 'Alessia',
+                avatar: 'img/img_05.jpg',
+                visible: true,
+                messages: [
+                {
+                date: '11/02/2021 14:30:55',
+                text: 'Il nostro rapporto non funziona più, finisce qua!',
+                status: 'received'
+                },
+                {
+                date: '11/02/2021 14:32:00',
+                text: 'LUL',
+                status: 'sent'
+                }
+                ],
+            },
+
+            ]
 
     },
     
